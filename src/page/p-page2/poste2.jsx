ef68596693e7547e6b4d44ffac02399d108dbabc
rafikln <rafiklongar000@gmail.com>
2024-06-18 23:25:44 -0700

    deux miss a j

--- src/page/p-page2/poste2.jsx
+++ src/page/p-page2/poste2.jsx
@@ -394,7 +394,7 @@ function Poste2({ USER }) {
                 <div className="w-72 bg-white max-w-xs mx-auto rounded-sm overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 transform hover:scale-100 cursor-pointer">
                   <div className="h-20 bg-red-400 flex items-center justify-between">
                     <p className="mr-0 text-white text-lg pl-5">
-                      Number Agents
+                      Number Agent
                     </p>
                   </div>
                   <div className="flex justify-between px-5 pt-6 mb-2 text-sm text-gray-600">
